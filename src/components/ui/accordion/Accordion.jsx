a546f19699d1f38b1ba6d3ec9fa5b0719f7c1b20
OxygenO22 <bamaik@mail.ru>
2023-05-25 16:23:29 +0300

    Fixed behavior of accordion

--- src/components/ui/accordion/Accordion.jsx
+++ src/components/ui/accordion/Accordion.jsx
@@ -1,18 +1,24 @@
 import  './Accordion.scss';
 import { AccordionData } from './AccordionData';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export const Accordion = () => {
   const [isShow, setIsShow] = useState(true);
   const [accordionId, setAccordionId] = useState(1);
+
+  useEffect(() => {
+    if (isShow) {
+      setIsShow(!isShow);
+    };
+  }, [accordionId, isShow] )
+
   return (
     
     AccordionData.map(data => 
         <div className='accordion__wrapper' key={data.id}>
         <div className='accordion__inner'>
           <div className='accordion__head' onClick={() => {
-            setIsShow(isShow => !isShow);
             setAccordionId(data.id)
             }}>
             <div className='accordion__head-content'>
@@ -20,12 +26,13 @@ export const Accordion = () => {
               <h3 className='accordion__head-title'>{data.title}</h3>
             </div>
           </div>
-          <div 
-            className={`accordion__content ${isShow && accordionId === data.id ? 'active' : ''}`}>
+          {
+            <div 
+            className={`accordion__content ${accordionId === data.id && 'active'} `}>
               <p className='accordion__content-text'>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum mollitia nobis quaerat, autem sed quia fuga saepe necessitatibus totam odio. Velit delectus, alias quaerat consectetur quos sequi dicta voluptates quas consequuntur quis neque deleniti minus ducimus aspernatur cum error tenetur impedit soluta id nostrum reiciendis, veniam, aliquam facere! Excepturi, amet?
               </p>
-          </div>
+          </div>}
         </div>
       </div>
     )
