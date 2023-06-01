f23c37ee71d871313ca9992c04e5743084db8144
OxygenO22 <bamaik@mail.ru>
2023-06-01 23:09:55 +0300

    Try to fix ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { Router } from "./components/ui/Router";
-import styles from "./App.module.scss";
+import React from 'react';
+import { Router } from './components/ui/Router';
+import styles from './App.module.scss';
 
 export const App = () => {
   return (
     <div className={styles.app}>
       <Router />
     </div>
-  );
-};
+  );};
